e9a44ec244eb5c50f5924aadcda5677fda0e13bc
Norihisa Yanai <llc.yanyan@gmail.com>
2023-01-01 10:32:26 +0900

    initial commit

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, Response, jsonify, render_template, logging, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
